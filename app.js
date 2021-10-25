2a4939ab9185ce36b616b2cf1eee9fde69a196a8
SachinthaMadhushanka <e17100@eng.pdn.ac.lk>
2021-10-25 18:38:16 +0530

    changed

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-
-    res.setHeader("X-Frame-Options", "SAMEORIGIN");
-    res.setHeader("X-Content-Type-Options", "nosniff");
-
     next();
 });
 
