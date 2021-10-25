519bed46b7795c1f767d337dd6ff458b76bf4787
SachinthaMadhushanka <e17100@eng.pdn.ac.lk>
2021-10-25 18:36:42 +0530

    changed

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
+    res.setHeader("X-Frame-Options", "SAMEORIGIN");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+
     next();
 });
 
