761dfce05436566af4484843a673fb07dc3041cd
SachinthaMadhushanka <e17100@eng.pdn.ac.lk>
2022-11-16 13:55:26 +0530

    MQTT

--- configs/configs.js
+++ configs/configs.js
@@ -12,8 +12,8 @@ exports.mqtt_options = {
 
 exports.petFeederIdPrev = "ffffffffffffff";
 
-exports.API_URL = "http://localhost:8000:";
-// export const API_URL = "https://smart-pet-feeder-backend.herokuapp.com";
+// exports.API_URL = "http://localhost:8000:";
+export const API_URL = "https://smart-pet-feeder-backend.herokuapp.com";
 
 let mqtt_client = mqtt.connect("mqtt://test.mosquitto.org", {clientId:"mqtt-tester12345"});
 
